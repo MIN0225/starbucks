d3bafe4d453c2ef6968ee3ec793db81706fabdd7
MIN0225 <cro77@naver.com>
2022-01-23 06:06:23 +0900

    Swiper error

--- js/main.js
+++ js/main.js
@@ -20,14 +20,33 @@ window.addEventListener('scroll', _.throttle(function() {
   console.log(window.scrollY);
   if (window.scrollY > 500) {
     // 배지 숨기기
-    badgeEl.style.display = 'none';
     // gsap.to(요소, 지속시간, 옵션);
-    // gsap.to(badgeEl, .6, {
-    //   opacity: 0
-    // });
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: 'none'
+    });
   } else {
     // 배지 보이기
-    badgeEl.style.display = 'block';
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display: 'block'
+    })    
   }
 }, 300));
 // _.throttle(함수, 시간)
+
+
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function (fadeEl, index) {
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * .7,
+    opacity: 1
+  });
+});
+
+// new Swiper(선택자, 옵션)
+new Swiper('.notice-line .swiper-container', {
+  direction: 'vertical',
+  // autoplay: true,
+  // loop: true
+});
